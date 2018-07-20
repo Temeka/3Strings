a6c87f768205dcdc24375ebe2141399e33f61081
Temeka <misstemeka1@live.com>
2018-07-20 18:30:21 -0500

    Add project files.

---
+++ 3Strings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _3Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello World!");
+            */
+
+            Console.Write("Enter your name prefix i.e., Mr., Mrs., etc.: ");
+            string s1 = Console.ReadLine();
+            Console.Write("Enter your first name: ");
+            string s2 = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string s3 = Console.ReadLine();
+
+            
+            Console.WriteLine( s1 + " " + s2  + " "+ s3);
+            Console.ReadLine();
+
+
+                                    
+
+            
+
+            
+           
+
+
+
+
+            
+
+
+
+        }
+    }
+}
